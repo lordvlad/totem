02186e4e7544e4206981aaa1bd17f107197bacaa
Waldemar Reusch <waldemar.reusch@googlemail.com>
2023-05-15 22:46:52 +0200

    set up eslint in vite

--- index.js
+++
@@ -1,4 +0,0 @@
-import { createServer } from 'http';
-import { app } from './app';
-const server = createServer((req, res) => app(req, res));
-server.listen(3000);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import Markdown from '@pity/vite-plugin-react-markdown'
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [
     preact(),
+    checker({ typescript: true, }),
     Markdown()
   ]
 })

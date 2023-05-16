a96b0ab9993ac623735b0937d1d7d647d0fcdc21
Waldemar Reusch <waldemar.reusch@googlemail.com>
2023-05-16 22:57:55 +0200

    improve footer behavior

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import './app.css'
 
 import { Grid, Link, Page, PageProps, Spacer } from '@geist-ui/core'
-import { Github } from '@geist-ui/icons'
+import { AlertTriangle, Github } from '@geist-ui/icons'
 
 import { Header } from './composites/Header.js'
 import { Help } from './composites/Help'
@@ -28,13 +28,18 @@ export function App(props: PageProps) {
       </Page.Content>
 
       <Page.Footer>
-        <Grid.Container>
+        <Grid.Container gap={3}>
           <Grid>
-            <Github />
+            <Link color href="http://github.com/lordvlad/totem" icon className="with-icon">
+              <Github />
+              {i18n`Check it out on github`}
+            </Link>
+
           </Grid>
           <Grid>
-            <Link color href="http://lordvlad.github.com" icon>
-              &nbsp;{i18n`Check me out on github`}
+            <Link color href="https://github.com/lordvlad/totem/issues" icon className="with-icon">
+              <AlertTriangle />
+              {i18n`File an issue`}
             </Link>
           </Grid>
         </Grid.Container>

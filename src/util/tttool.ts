f1365e8cd7f0ae059d3195d58c3701e54cbaa1c7
Waldemar Reusch <waldemar.reusch@googlemail.com>
2023-05-16 22:35:21 +0200

    Fix broken ci test

--- src/util/tttool.ts
+++ src/util/tttool.ts
@@ -1,11 +1,10 @@
-import { mkdir, chmod } from "fs/promises";
+import { chmod, mkdir } from "fs/promises";
 import { tmpdir } from "os";
 import { join, sep } from "path";
 import { download } from "./download";
 import { execFile } from "./execFile";
 import { isFile } from "./isFile";
 import { unzip } from "./unzip";
-import { execSync } from "child_process";
 
 const version = "1.11"
 const tttoolDir = join(tmpdir(), "totem-tttool")
@@ -17,14 +16,9 @@ async function init() {
     await mkdir(tttoolDir, { recursive: true })
     await download(url, join(tttoolDir, "tttool.zip"))
     await unzip(join(tttoolDir, "tttool.zip"), tttoolDir)
-    if (sep === '/') await chmod(tttoolPath, '777')
     if (sep === '/') {
-        // FIXME remove this block
-        console.log("ls -lash: ")
-        console.log(execSync(`ls -lash ${join(tttoolDir, `tttool-${version}`)}`).toString("utf-8"))
-    } else {
-        console.log("dir: ")
-        console.log(execSync(`dir ${join(tttoolDir, `tttool-${version}`)}`).toString("utf-8"))
+        await chmod(tttoolPath, 0o755)
+        await chmod(join(tttoolDir, `tttool-${version}`, 'linux', 'ttool'), 0o755)
     }
     console.log("Downloaded and unpacked tttool to", tttoolDir)
 }

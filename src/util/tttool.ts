37e49d5690eb6871dbead93d15d88d3e947ab0e5
Waldemar Reusch <waldemar.reusch@googlemail.com>
2023-05-16 22:15:57 +0200

    debug failing ci test

--- src/util/tttool.ts
+++ src/util/tttool.ts
@@ -5,10 +5,11 @@ import { download } from "./download";
 import { execFile } from "./execFile";
 import { isFile } from "./isFile";
 import { unzip } from "./unzip";
+import { execSync } from "child_process";
 
 const version = "1.11"
 const tttoolDir = join(tmpdir(), "totem-tttool")
-const tttoolPath = join(tmpdir(), "totem-tttool", `tttool-${version}`, sep === '/' ? 'tttool' : 'tttool.exe')
+const tttoolPath = join(tttoolDir, `tttool-${version}`, sep === '/' ? 'tttool' : 'tttool.exe')
 const url = `https://github.com/entropia/tip-toi-reveng/releases/download/${version}/tttool-${version}.zip`
 
 async function init() {
@@ -17,6 +18,14 @@ async function init() {
     await download(url, join(tttoolDir, "tttool.zip"))
     await unzip(join(tttoolDir, "tttool.zip"), tttoolDir)
     if (sep === '/') await chmod(tttoolPath, '777')
+    if (sep === '/') {
+        // FIXME remove this block
+        console.log("ls -lash: ")
+        console.log(execSync(`ls -lash ${join(tttoolDir, `tttool-${version}`)}`))
+    } else {
+        console.log("dir: ")
+        console.log(execSync(`dir ${join(tttoolDir, `tttool-${version}`)}`))
+    }
     console.log("Downloaded and unpacked tttool to", tttoolDir)
 }
 

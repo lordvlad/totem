949ad7af420cc68667f3944ced6ed64d5ad78846
Waldemar Reusch <lordvlad@users.noreply.github.com>
2025-10-04 23:33:13 +0200

    Update ThemePicker.tsx

--- src/components/ThemePicker.tsx
+++ src/components/ThemePicker.tsx
@@ -1,5 +1,5 @@
 import { Button, useMantineColorScheme } from "@mantine/core";
-import { useCallback, useEffect } from "react";
+import { useMemo } from "react";
 import Moon from "../components/icons/Moon";
 import Sun from "../components/icons/Sun";
 import { useI18n } from "../hooks/useI18n/useI18n";
@@ -16,20 +16,15 @@ export function ThemePicker() {
   const i18n = useI18n();
   const { colorScheme, setColorScheme } = useMantineColorScheme();
 
-  useEffect(() => {
-    setTimeout(() => setColorScheme("auto"), 0);
-    setTimeout(() => setColorScheme(colorScheme), 0);
-  }, []);
-
   const toggle = useCallback(() => {
     setColorScheme(
       ({ light: "dark", dark: "auto", auto: "light" } as const)[colorScheme],
     );
   }, [colorScheme]);
 
-  const labels = useCallback(
-    (c: "light" | "dark" | "auto") => {
-      return { light: i18n`light`, dark: i18n`dark`, auto: i18n`auto` }[c];
+  const labels = useMemo(
+    () => {
+      return { light: i18n`light`, dark: i18n`dark`, auto: i18n`auto` } as const;
     },
     [i18n],
   );
@@ -41,7 +36,7 @@ export function ThemePicker() {
       onClick={toggle}
       leftSection={icons[colorScheme]}
     >
-      {labels(colorScheme)}
+      {labels[colorScheme]}
     </Button>
   );
 }
